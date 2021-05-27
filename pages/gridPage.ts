dc381ef22ef6bba717afe8279c6447f549a11154
Mithun Kumar <mithun5587@gmail.com>
2021-05-27 15:54:49 +0530

    Feat:  Adding New Tests

--- pages/gridPage.ts
+++ pages/gridPage.ts
@@ -26,4 +26,28 @@ export class GridPage {
   public static get columnHeader() {
     return $$('[aria-rowindex="1"] > [role="columnheader"]');
   }
-}
+
+  public static get PrimaryColumnHeader() {
+    return $$('div.ag-header-group-cell-label span');
+  }
+
+  public static get PrimaryColumn() {
+    return $$('d.ag-header-row-column-group');
+  }
+  
+  public static get Pivot() {
+    return $$('.ag-pivot-mode-select');
+  }
+  public static get pivotPanelContainer() {
+    return $$('span.ag-virtual-list-container');
+  }
+
+  public static get secondaryPanel() {
+    return $$('span.ag-header-cell-text[unselectable]');
+  }
+
+  public static get secondarySidePanelValue() {
+    return $$('.div.ag-column-select-virtual-list-item');
+  }
+  
+};
--- tests/grid.test.ts
+++ tests/grid.test.ts
@@ -19,4 +19,58 @@ describe("Load AG Grid", () => {
       expect(el.isDisplayed()).to.eql(true);
     });
   });
+
+  it("Should display Primary column labels", () => {
+    GridPage.mainGrid.waitForDisplayed();
+
+    /**
+     * I'm using forEach here for readability purposes
+     * The function iterates through the element array
+     * and checks if Primary column header labels are following.
+     * Participant,Game of Choice,Performance,Monthly Breakdown
+     */
+     GridPage.PrimaryColumn.forEach((ee) => {
+      const text = ee.getText();
+      expect(text).to.include("Participant,Game of Choice,Performance,Monthly Breakdown")
+    });
+  });
+
+  it("Should display Primary column labels in side bar", () => {
+    GridPage.mainGrid.waitForDisplayed();
+
+    /**
+     * I'm using forEach here for readability purposes
+     * The function iterates through the element array
+     * and checks if Primary column headers are there in sidebar.
+     * Participant,Game of Choice,Performance,Monthly Breakdown
+     */
+     GridPage.pivotPanelContainer.forEach((ee) => {
+      ee.scrollIntoView();
+      const text = ee.getText();
+      expect(text).to.include("Participant,Game of Choice,Performance,Monthly Breakdown")
+    });
+  });
+
+  it("Should display Secondary column and are also available in side bar", () => {
+    GridPage.mainGrid.waitForDisplayed();
+
+    /**
+     * I'm using forEach here for readability purposes
+     * The function iterates through the element array
+     * and checks if Secondary column headers are dispalyed 
+     * Check Secondary Panel headers are available in side bar.
+     */
+     GridPage.secondaryPanel.forEach((ee) => {
+      var secondarySidebarValue
+      expect(ee.isDisplayed()).to.eql(true);
+      const secondarycolumnText=ee.getText();
+      //Checking values are also present in side bar
+      GridPage.secondarySidePanelValue.forEach ((elem) => {
+      expect(elem.isDisplayed()).to.eql(true);
+      secondarySidebarValue = elem.getText();
+      console.log('secondarySidebarValue', secondarySidebarValue);
+      expect (secondarycolumnText).to.equal(secondarySidebarValue);
+      });
+    });
+  });
 });
